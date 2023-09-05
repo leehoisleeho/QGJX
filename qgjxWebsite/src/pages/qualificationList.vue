34720160deea886610ae547732c3df3b9c3902e3
leeho <leehovip@gmail.com>
2023-09-05 19:42:22 +0800

    修改证书margin

--- qgjxWebsite/src/pages/qualificationList.vue
+++ qgjxWebsite/src/pages/qualificationList.vue
@@ -57,11 +57,11 @@ const open = (item) => {
 
 .item {
   width: 24%;
+  margin-bottom: 20px;
 }
 
 .item > img {
   width: 100%;
-  height: 480px;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   border-radius: 10px;
 }
